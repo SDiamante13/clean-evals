cbeaba16ec59daeb83bc874e78ab2b0251e166e5
Steven Diamante <stevendiamante13@gmail.com>
2026-01-31 00:36:28 -0500

    feat: US-011 - Implement trace-to-chronological-story flattener

--- src/distill-trace.ts
+++ src/distill-trace.ts
@@ -2,22 +2,14 @@ import { ReadableSpan } from '@opentelemetry/sdk-trace-base';
 import { findToolCalls } from './find-tool-calls.js';
 
 export function distillTrace(spans: ReadableSpan[]): string {
-  const toolSpans = spans
-    .filter((span) => hasToolName(span))
-    .sort((a, b) => compareStartTimes(a.startTime, b.startTime));
+  const toolSpans = spans.filter((span) => hasToolName(span)).sort((a, b) => compareStartTimes(a.startTime, b.startTime));
 
-  return toolSpans
-    .map((span, index) => formatStep(span, index + 1))
-    .join('\n');
+  return toolSpans.map((span, index) => formatStep(span, index + 1)).join('\n');
 }
 
 function hasToolName(span: ReadableSpan): boolean {
   const attrs = span.attributes;
-  return !!(
-    attrs['tool.name'] ||
-    attrs['gen_ai.request.function_call.name'] ||
-    attrs['llm.function_call.name']
-  );
+  return !!(attrs['tool.name'] || attrs['gen_ai.request.function_call.name'] || attrs['llm.function_call.name']);
 }
 
 function compareStartTimes(a: [number, number], b: [number, number]): number {
